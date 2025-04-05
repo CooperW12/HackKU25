e6a3cbf5cf4d4df7d7d315e8b6ef31f488202eba
evannoeth <evannoeth@gmail.com>
2025-04-05 11:22:15 -0500

    created a WORKING slider! we have extension :fire:

--- popup.js
+++ popup.js
@@ -2,18 +2,16 @@ let input = document.querySelector('input');
 
 input.addEventListener('change', e => setValue(e.target.value));
 
-async function setValue(value){
-    await browser.storage.local.set({value});
+async function setValue(value) {
+    await browser.storage.local.set({ value });
 }
 
-async function init(){
-    let {value} = browser.local.storage.get('value');
-    if (!value){
-        value = 0;
-    }
-
+async function init() {
+    let result = await browser.storage.local.get('value'); // Fixed: Correct API usage
+    let value = result.value || 0; // Default to 0 if undefined
+    
     input.value = value;
-    setValue(value);
+    await setValue(value); // Optional: Persist default value
 }
 
 init().catch(e => console.error(e));
