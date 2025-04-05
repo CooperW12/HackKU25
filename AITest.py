3d9a3a89ba9ebd5188edb25deafe3d60d39fa7b9
Cooper Wright <73917610+CooperW12@users.noreply.github.com>
2025-04-05 11:22:16 -0500

    navigating sites?

--- AITest.py
+++ AITest.py
@@ -3,13 +3,29 @@ import os
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
+file = open("testweb.txt", "r")
+htmlCode = file.read()
+
 client = OpenAI(api_key=os.environ['API_KEY'], base_url="https://api.deepseek.com")
+
+systemPrompt = """
+The user will provide you with html code of a webpage and instuctions on where they wish to go to.
+
+Please find the html element they need to click on and ONLY output the css selector.
+
+EXAMPLE JSON OUTPUT:
+{
+    "target_element": [FILL HERE]
+}
+"""
+
+instruction = "I want to look at my data structures and algorithms class"
 
 response = client.chat.completions.create(
     model = "deepseek-chat",
     messages = [
-        {"role": "system", "content": "you are helpful assistant"},
-        {"role": "user", "content": "Hello"},
+        {"role": "system", "content": systemPrompt},
+        {"role": "user", "content": "INSTRUCTION: " + instruction + "CODE: " + htmlCode},
         ],
         stream = False
 )
