eda427ee52fca3b6449d27076e81aa39c5268f55
evannoeth <evannoeth@gmail.com>
2025-04-05 23:57:22 -0500

    you too

--- content_script.js
+++ content_script.js
@@ -1,13 +1,18 @@
-//Listen for messages from popup
-console.log("Content script loaded!"); //Shows up in web console
 
-browser.runtime.onMessage.addListener((message) => {
+// Listen for messages from popup
+console.log("Content script loaded!");
+
+browser.runtime.onMessage.addListener(async (message) => {
     if (message.action === "userPrompt") {
         console.log("Friendly Neighborhood User: ", message.prompt);
+        
+        // Get the HTML of the current page
+        
+        // Send back to popup.js
     }
     else if (message.action === "apiResponse") {
         console.log("A message from far, far away: ", message.data.response);
-        //basically do something with the message we recieve from the heroku server
+        // Do something with the message from the heroku server
     }
     return Promise.resolve("Message received");
 });
